abdb6f2902ba6860e67a24cc4037b118d1238131
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-10-29 14:45:08 -0500

    Handle change on input and select; demo circle to place

--- components/Options.js
+++ components/Options.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import Circle from './Circle.js';
+import Star from './Star.js';
 
-const Options = () => {
+const SHAPES = ['circle', 'star'];
+
+const Options = (props) => {
+  const { handleChange, sizeValue, shapeValue } = props;
+  const shapeToDemo = shapeValue.toUpperCase();
   return (
     <div id="options-bar">
-      <div className="option">
-	<label htmlFor="radius-range">Size</label>
-	<input type="range" className="slider" id="radius-range"
-	       min="1" max="30"
-	       value="15" 
-	       onChange={() => console.log('todo: show example size')}
-	/>
+    <div className="option">
+    <label htmlFor="size">Size</label>
+    <input type="range" className="slider" id="radius-range"
+	   min="1" max="50" step="1"
+	   name="size"
+	   onChange={handleChange}
+	   value={sizeValue}
+    />
       </div>
       <div className="option">
-	<label htmlFor="shapes">Shape</label>
-	<select onChange={() => console.log('todo: pass choice to Space')}>
-	  <option value="circle">Circle</option>
-	  <option value="square">Square</option>
+	<label htmlFor="shape">Shape</label>
+	<select name="shape"
+		onChange={handleChange}>
+	  {SHAPES.map(shape =>
+	  (<option value={`${shape}`}>{shape}</option>)
+	  )}
 	</select>
       </div>
-
+      <div className="demo">
+	{(shapeValue === 'circle') ? <Circle x={50} y={50} r={sizeValue} /> : <Star />}
+      </div>
     </div>
   )
 };
