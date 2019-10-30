4e24704085b4f6e5dcf08f110e914b3e8b602334
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-10-30 16:22:10 -0500

    more divs for styling

--- components/Options.js
+++ components/Options.js
@@ -9,33 +9,35 @@ const Options = (props) => {
   const shapeToDemo = shapeValue.toUpperCase();
   return (
     <div id="options-bar">
-    <div className="option">
-    <label htmlFor="size">Size</label>
-    <input type="range" className="slider" id="radius-range"
-	   min="1" max="50" step="1"
-	   name="size"
-	   onChange={handleChange}
-	   value={sizeValue}
-    />
-      </div>
-      <div className="option">
-	<label htmlFor="shape">Shape</label>
-	<select name="shape"
-		onChange={handleChange}>
-	  {SHAPES.map((shape,index) =>
-	    (<option key={index}
-		     value={`${shape}`}>
-	      {shape}
-	    </option>)
-	  )}
-	</select>
-      </div>
-      <div className="option">
-	<div className="demo">
-	  {(shapeValue === 'circle') ? <Circle x={60} y={70} r={sizeValue} /> : <Square x={60} y={70} r={sizeValue} />}
+      <div className="option-col">
+	<div className="option">
+	  <label htmlFor="size">Size</label>
+	  <input type="range" className="slider" id="radius-range"
+		 min="1" max="50" step="1"
+		 name="size"
+		 onChange={handleChange}
+		 value={sizeValue}
+	  />
+	</div>
+	<div className="option">
+	  <label htmlFor="shape">Shape</label>
+	  <select name="shape"
+	    onChange={handleChange}>
+	    {SHAPES.map((shape,index) =>
+	      (<option key={index}
+		       value={`${shape}`}>
+		{shape}
+	      </option>)
+	    )}
+	  </select>
 	</div>
       </div>
-    </div>
+	<div className="option">
+	  <div className="demo">
+	    {(shapeValue === 'circle') ? <Circle x={60} y={70} r={sizeValue} /> : <Square x={60} y={70} r={sizeValue} />}
+	  </div>
+	</div>
+      </div>
   )
 };
 
