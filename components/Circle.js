31b31bc65d89f20bb37e46ea008d4008653a97e3
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-10-30 16:11:33 -0500

    add radial gradient, playing around with speed

--- components/Circle.js
+++ components/Circle.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const Circle = (props) => {
-  const { x, y, r } = props;
+  const { x, y, r, dx, dy, t } = props;
+  const v_x = s => s + s;
+  const a_x = s => 0;
+  const v_y = s => Math.sin(t) * s/20;
   return (
     <svg>
-      <circle cx={x} cy={y} r={props.r || 10} fill="yellow" />
+      <defs>
+	<radialGradient id="RadialGradient1">
+          <stop offset="30%" stopColor="yellow"/>
+          <stop offset="500%" stopColor="transparent"/>
+	</radialGradient>
+      </defs>
+      <circle cx={x} cy={y} r={r || 10} fill="url(#RadialGradient1)" />
     </svg>
   )
 };
