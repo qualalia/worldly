e760303d4e0ab375c0e30e9d39348cc45df6c1d0
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-10-31 16:07:04 -0500

    gaussian blur instead of radial gradient

--- components/Square.js
+++ components/Square.js
@@ -1,24 +1,31 @@
 import React from 'react';
 
-const Square = (props) => {
-  const { x, y, r, theta } = props;
-  const rotation = `rotate(${theta % 360}, ${x + r/2}, ${y + r/2})`;
-  return (
-    <svg>
-      <defs>
-	<radialGradient id="RadialGradient1">
-          <stop offset="40%" stopColor="purple"/>
-          <stop offset="100%" stopColor="transparent"/>
-	</radialGradient>
-      </defs>
-      {/*<circle cx={x+5} cy={y+5} r={r || 10}
-	    fill="url(#RadialGradient1)"/>*/}
-      <rect x={x} y={y}
-	    width={r || 20} height={r || 20}
-	    transform={rotation}
-	    fill="purple"/>
-    </svg>
-  )
-};
-
-export default Square;
+export default class Square extends React.PureComponent {
+  render () {
+    const { x, y, r, theta, fill, fillOpacity } = this.props;
+    const rotation = `rotate(${theta % 360}, ${x + r/2}, ${y + r/2})`;
+    return (
+      <svg>
+	<defs>
+	  <filter id="dropshadow" height="120%">
+	    <feGaussianBlur in="SourceGraphic" stdDeviation="4"/>
+	    <feComponentTransfer>
+	      <feFuncA type="linear" slope="0.9"/>
+	    </feComponentTransfer>
+	    <feMerge>
+	      <feMergeNode />
+	      <feMergeNode in="SourceGraphic"/>
+	    </feMerge>
+	  </filter>
+	</defs>
+	<rect x={x} y={y}
+	      width={r || 20} height={r || 20}
+	      transform={rotation}
+	      filter="url(#dropshadow)"
+	      fill="coral"
+	      fillOpacity={fillOpacity}
+	/>
+      </svg>
+    )
+  }
+}
