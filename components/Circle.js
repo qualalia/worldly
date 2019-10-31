19a06294f86f04cab4fe549bb07a2c8a1646a680
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-10-31 12:21:22 -0500

    handles fill and fillOpacity props

--- components/Circle.js
+++ components/Circle.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 export default class Circle extends React.PureComponent {
   render () {
-    const { x, y, r } = this.props;
+    const { x, y, r, fill, fillOpacity } = this.props;
     const v_x = s => s + s;
     const a_x = s => 0;
     return (
       <svg>
 	<defs>
 	  <radialGradient id="RadialGradient1">
-            <stop offset="70%" stopColor="blue" />
+            <stop offset="70%" stopColor="white" />
             <stop offset="100%" stopColor="transparent" />
 	  </radialGradient>
 	</defs>
-	<circle cx={x} cy={y} r={r || 10} fill="url(#RadialGradient1)" fillOpacity="0.2" />
+	<circle cx={x} cy={y} r={r || 10} fill={fill || "url(#RadialGradient1)"} fillOpacity={fillOpacity || 1} />
       </svg>
     )
   }
