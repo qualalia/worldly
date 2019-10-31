dda45e1ba388b843f6b12ccb716b807b17e684c9
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-10-30 22:12:33 -0500

    now a PureComponent; cleanup

--- components/Circle.js
+++ components/Circle.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
-const Circle = (props) => {
-  const { x, y, r, dx, dy, t } = props;
-  const v_x = s => s + s;
-  const a_x = s => 0;
-  const v_y = s => Math.sin(t) * s/20;
-  return (
-    <svg>
-      <defs>
-	<radialGradient id="RadialGradient1">
-          <stop offset="30%" stopColor="midnightblue" />
-          <stop offset="100%" stopColor="transparent" />
-	</radialGradient>
-      </defs>
-      <circle cx={x} cy={y} r={r || 10} fill="url(#RadialGradient1)" fillOpacity="0.5" />
-    </svg>
-  )
-};
-
-export default Circle;
+export default class Circle extends React.PureComponent {
+  render () {
+    const { x, y, r } = this.props;
+    const v_x = s => s + s;
+    const a_x = s => 0;
+    return (
+      <svg>
+	<defs>
+	  <radialGradient id="RadialGradient1">
+            <stop offset="70%" stopColor="blue" />
+            <stop offset="100%" stopColor="transparent" />
+	  </radialGradient>
+	</defs>
+	<circle cx={x} cy={y} r={r || 10} fill="url(#RadialGradient1)" fillOpacity="0.2" />
+      </svg>
+    )
+  }
+}
