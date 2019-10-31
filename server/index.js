a9e71bd6a0b7f64f60ccc3f74fc228e059c6a117
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-10-31 13:14:10 -0500

    build and connect to express server

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+const router = express.Router();
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
+router.get('/', (req, res, next) => {
+  try {
+    res.sendFile(path.join(__dirname, '../app.js'));
+  }
+  catch (err) {
+    next(err);
+  }
+});
+
+router.get('/', (req, res, next) => {
+  try {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  }
+  catch (err) {
+    next(err);
+  }
+});
+
+/*router.get('/stickers', (req, res, next) => {
+  try {
+    res.sendFile(path.join(__dirname, '../components/'));
+  }
+  catch (err) {
+    next(err);
+  }
+});*/
+
+router.use((req, res, next) => {
+  const err = new Error('Route not found!')
+  err.status = 404
+  next(err)
+})
+
+app.use((err, req, res) => {
+  res.status(err.status || 500).send(err.message || 'Internal server error')
+});
+
+app.listen(8080, () => console.log('Listening on port 8080'));
+
+module.exports = app;
