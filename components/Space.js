d3aebfb2e32bf3a3ee3b9be0fee56dc707a5abd9
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-10-31 12:24:49 -0500

    add lil dude to move around with arrow keys or WASD, remove Stickers stuff

--- components/Space.js
+++ components/Space.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import Circle from './Circle.js';
-import Square from './Square.js';
 import Planet from './Planet.js';
+
+const celestialBkg = [];
+const testBodies = [
+  { x: 400, y: 180 },
+  { x: 800, y: 180 },
+  { x: 1200, y: 180 },
+  { x: 400, y: 540 },
+  { x: 800, y: 540 },
+  { x: 1200, y: 540 },
+];
+
+for (let i = 0; i < 500; i++)
+  celestialBkg.push({ x: Math.random() * 5000, y: Math.random() * 900 });
 
 export default class Space extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      /*      x: -20,
-	 y: 0,
-	 r: props.size,
-	 dx: 0,
-	 dy: 0,
-	 circles: [],
-	 squares: [], */
-      circles: [],
+      foreground: testBodies,
+      background: celestialBkg,
       t: 0,
       px: 700,
       py: 400,
@@ -25,10 +31,6 @@ export default class Space extends React.Component {
       () => this.tick(),
       16
     );
-    const celestialBkg = [];
-    for (let i = 0; i < 500; i++) 
-      celestialBkg.push({x: Math.random() * 5000, y: Math.random() * 900 });
-    this.setState({ circles: celestialBkg });
   }
   componentWillUnmount() {
     clearInterval(this.timerId);
@@ -46,98 +48,61 @@ export default class Space extends React.Component {
     else if (['ArrowDown', 'o', 's'].includes(event.key))
       dy = 10;
     else {};
+    const bkgParallax = this.state.background.map(circle => ({
+      x: circle.x - (20), y: circle.y - (20)
+    }));
+
     this.setState({
       px: this.state.px + dx,
       py: this.state.py + dy,
+      background: [
+	...this.state.background,
+	bkgParallax,
+      ],
     });
+    
   }
-
-  handleMouseMove = (evt) => {
-    /*const dx = (evt.clientX - this.state.x)
-       ? (evt.clientX - this.state.x)
-       : 0.1;
-       const dy = (evt.clientY - this.state.y)
-       ? (evt.clientY - this.state.y)
-       : 0.1;
-       const dtheta = 0;
-       this.setState({
-       x: evt.clientX - 10,// both -10 with no margins, (-45, -30) with 2rem
-       y: evt.clientY - 10,
-       dx: dx,
-       dy: dy,
-       r: this.props.size,
-       });*/
+  handleKeyUp = (e) => {
+    console.log('key up');
   }
+  
   handleClick = (evt) => {
-    console.log('in focus')
-    /*if (this.props.shape === 'circle') {
-       this.setState({
-       circles: [
-       ...this.state.circles,
-       { x: this.state.x,
-       y: this.state.y,
-       r: this.state.r,
-       dx: this.state.dx,
-       dy: this.state.dy,
-       },
-       ],
-       });
-       }
-       else {//if (this.props.shape === 'square') {
-       this.setState({
-       squares: [
-       ...this.state.squares,
-       { x: this.state.x,
-       y: this.state.y,
-       r: this.state.r,
-       theta: this.state.t,
-       },
-       ],
-       });
-       }*/
+    console.log('in focus');
   }
   
   tick() {
     this.setState({
-      t: this.state.t + 0.007,
+      t: this.state.t + 0.02,
     });
   }
   render() {
-    const { px, py, t, circles } = this.state;
+    const { px, py, t, background, foreground } = this.state;
     return (
       <div tabIndex="1"
 	   className="svg-container"
 	   onMouseMove={this.handleMouseMove}
 	   onClick={this.handleClick}
 	   onKeyDown={this.handleKeyDown}
+	   onKeyUp={this.handleKeyUp}
       >
 	<svg>
 	  <Planet x={px} y={py} t={t} />
-	  {circles.map((circle, index) =>
+	  {/*<rect x={0} y={0} width="100%" height="100%" fill="darkmagenta" fillOpacity="0.1" />*/}
+	  {background.map((circle, index) =>
 	    <Circle key={index}
 		    x={circle.x}
 		    y={circle.y}
+		    fillOpacity={0.5}
 	    />
 	  )}
-	  {/* {(shapeToPreview === 'circle')
-	      ? <circle cx={x} cy={y} r={r} fill="transparent" />
-	      : <rect x={x} y={y} width={r} height={r} fill="transparent" />
-	      }
-	      {circles.map((circle,index) =>
-	      <Circle key={index}
-	      x={circle.x} y={circle.y}
-	      r={circle.r} t={t}
-	      dx={circle.dx} dy={circle.dy}
-	      />
-	      )}
-	      {squares.map((square,index) =>
-	      <Square key={index}
-	      x={square.x} y={square.y}
-	      r={square.r}
-	      theta={square.theta + t}
-	      />
-	      )} */}
-	  <rect x={0} y={0} width="100%" height="100%" fill="darkmagenta" fillOpacity="0.2" />
+	  {foreground.map((el, index) =>
+	    <Circle key={index}
+		    x={el.x}
+		    y={el.y}
+		    r={15}
+		    fill={"orange"}
+	    />
+	  )}
 	</svg>
       </div>
     )
