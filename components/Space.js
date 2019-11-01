72390e29926816628f0121e1e1ecbe17e1d61cea
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-10-31 20:58:52 -0500

    toying with movement

--- components/Space.js
+++ components/Space.js
@@ -5,11 +5,7 @@ import Planet from './Planet.js';
 const celestialBkg = [];
 const testBodies = [
   { x: 400, y: 180 },
-  { x: 800, y: 180 },
-  { x: 1200, y: 180 },
-  { x: 400, y: 540 },
   { x: 800, y: 540 },
-  { x: 1200, y: 540 },
 ];
 for (let i = 0; i < 200; i++)
   celestialBkg.push({ x: Math.random() * 4000, y: Math.random() * 900 });
@@ -24,8 +20,8 @@ export default class Space extends React.Component {
       foreground: testBodies,
       background: celestialBkg,
       t: 0,
-      x: width/2, //+ Math.cos(0) * Math.cos(0) * 2,
-      y: height/2, //+ Math.cos(0) * Math.sin(0) * 2,
+      x: width/2,
+      y: height/2,
       up: false,
       down: false,
       left: false,
@@ -47,26 +43,19 @@ export default class Space extends React.Component {
   }
   handleKeyDown = (e) => {
     const event = e || window.e;
-    let dx = 0;
-    let dy = 0;
     if (['ArrowLeft', 'a'].includes(event.key)) {
-      dx = -10;
       this.setState({ left: true });
     }
     else if (['ArrowRight', 'd', 'e'].includes(event.key)) {
-      dx = 10;
       this.setState({ right: true });
     }
     else if (['ArrowUp', 'w', ','].includes(event.key)) {
-      dy = -10;
       this.setState({ up: true });
     }
     else if (['ArrowDown', 'o', 's'].includes(event.key)) {
-      dy = 10;
       this.setState({ down: true });
     }
     else {};
-
     /*    this.setState({
        px: this.state.px + dx,
        py: this.state.py + dy,
@@ -75,47 +64,70 @@ export default class Space extends React.Component {
        bkgParallax,
        ],
        });*/
-    
   }
 
   tick() {
     const { x, y, left, right, up, down, t } = this.state;
     if (left) {
-      this.v_x = -t;
-      this.a_x = 0.05;
+      this.a_x = 0.3;
+      this.v_x += Math.max(-1 + this.a_x, -5);
+    }
+    else if (!left) {
+      this.v_x += -0.5 * this.a_x / Math.max(this.v_x, 2);
     }
     
     if (right) {
-      this.v_x = Math.log(t) / Math.sqrt(t);
-      this.a_x = 0;
+      this.a_x = -0.5;
+      this.v_x += 1 + this.a_x;
+    }
+    else if (!right) {
+      this.v_x += 0.5 * this.a_x;
     }
 
     if (up) {
-      this.v_y = -t/2;
       this.a_y = 0.2;
+      this.v_y = -1 + this.a_y;
+    }
+    else if (!up) {
+      this.v_y += -0.2 * this.a_y;
     }
 
     if (down) {
-      this.v_y = t/2;
       this.a_y = -0.2;
+      this.v_y = 1;
+    }
+    else {
+      this.v_y += 0.2 * this.a_y;
     }
     
-    if (x >= width || x <= 0) {
+    if (x >= width-5 || x <= 15) {
       this.v_x = -1 * this.v_x;
     }
-    if (y >= height || y <= 0) {
+    if (y >= height || y <= 10) {
       this.v_y = -1 * this.v_y;
     }
-
-    this.setState({
-      t: t + 0.02,
-      x: this.state.x + this.v_x + this.a_x,
-      y: this.state.y + this.v_y + this.a_y,
-      left: false,
-      right: false,
-      up: false,
-      down: false,
-    });
+    if (this.v_x === 0 && this.v_y === 0) {
+      this.setState({
+	t: t + 0.02,
+	x: this.state.x + Math.cos(t*4) * Math.cos(t) * x/500,
+	y: this.state.y + Math.cos(t*4) * Math.sin(t) * y/500,
+	left: false,
+	right: false,
+	up: false,
+	down: false,
+      })
+    }
+    else {
+      this.setState({
+	t: t + 0.02,
+	x: this.state.x + this.v_x + this.a_x,
+	y: this.state.y + this.v_y + this.a_y,
+	left: false,
+	right: false,
+	up: false,
+	down: false,
+      });
+    }
   }
   render() {
     const { x, y, t, background, foreground } = this.state;
@@ -127,8 +139,7 @@ export default class Space extends React.Component {
 	   onKeyDown={this.handleKeyDown}
 	   onKeyUp={this.handleKeyUp}
       >
-	<svg width={window.innerWidth} height={window.innerHeight}>
-	  {/*<rect x={0} y={0} width="100%" height="100%" fill="darkmagenta" fillOpacity="0.1" />*/}
+	<svg width={width} height={height}>
 	  {background.map((circle, index) =>
 	    <Circle key={index}
 		    x={circle.x} y={circle.y}
@@ -142,10 +153,11 @@ export default class Space extends React.Component {
 		    x={el.x}
 		    y={el.y}
 		    r={15}
-		    fill={"orange"}
+		    fill={"yellow"}
 	    />
 	  )}
-	  <Planet x={x} y={y} t={t} />
+	  <Planet x={x} y={y} t={t}
+		  v_x={this.v_x} v_y={this.v_y} />
 	</svg>
       </div>
     )
